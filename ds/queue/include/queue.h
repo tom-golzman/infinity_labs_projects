02be149f5d53b4c4dd915bdd70cb4e783d9bffbd
tom.golzman <tomgolzman@gmail.com>
2025-04-08 16:28:49 +0300

    Push files to 'queue'

--- ds/queue/include/queue.h
+++ ds/queue/include/queue.h
@@ -2,11 +2,20 @@
 #define __ILRD_QUEUE__
 
 /************************************includes************************************/
+#include <stddef.h>
 
 /************************************typedef************************************/
-
-/************************************define************************************/
+typedef struct queue queue_t;
+typedef int (*q_is_match_t)(const void* data1, const void* data2);
 
 /************************************Functions************************************/
+queue_t* QueueCreate(void);
+int QueueEnqueue(queue_t* queue, void* data);
+void QueueDequeue(queue_t* queue);
+void* QueuePeek(const queue_t* queue);
+int QueueIsEmpty(const queue_t* queue);
+size_t QueueSize(const queue_t* queue);
+void* QueueFind(const queue_t* queue, void* data, q_is_match_t is_match);
+void QueueDestroy(queue_t* queue);
 
 #endif
--- ds/queue/test/queue_test.c
+++ ds/queue/test/queue_test.c
@@ -1,6 +1,6 @@
 /************************************includes************************************/
-#include "queue.h"
 #include <stdio.h> /* printf */
+#include "queue.h"
 
 /************************************define************************************/
 #define GREEN "\033[1;32m"
@@ -10,13 +10,64 @@
 #define TITLE "\033[1;4m"
 
 /************************************Functions Forward Decleration************************************/
+static int IsMatch(const void* data1, const void* data2);
 
 /************************************main************************************/
 int main(void)
 {
+	queue_t* queue = QueueCreate();
+	int val1 = 10, val2 = 20, val3 = 30;
+	int* peeked_val = NULL;
+	size_t size = 0;
+	int* found = NULL;
 	
+	printf(TITLE "Enqueue first element:\n" RESET);
+	printf("Status: %d\n", QueueEnqueue(queue, &val1));
+	peeked_val = QueuePeek(queue);
+	printf("Peeked val: %d\n", *peeked_val);
+	size = QueueSize(queue);
+	printf("Queue size: %lu\n", size);
+	
+	printf(TITLE "\nEnqueue second element:\n" RESET);
+	printf("Status: %d\n", QueueEnqueue(queue, &val2));
+	peeked_val = QueuePeek(queue);
+	printf("Peeked val: %d\n", *peeked_val);
+	size = QueueSize(queue);
+	printf("Queue size: %lu\n", size);
+	
+	printf(TITLE "\nEnqueue third element:\n" RESET);
+	printf("Status: %d\n", QueueEnqueue(queue, &val3));
+	peeked_val = QueuePeek(queue);
+	printf("Peeked val: %d\n", *peeked_val);
+	size = QueueSize(queue);
+	printf("Queue size: %lu\n", size);
+	
+	printf(TITLE "\nDequeue:\n" RESET);
+	QueueDequeue(queue);
+	peeked_val = QueuePeek(queue);	
+	printf("Peeked val after dequeue: %d\n", *peeked_val);
+	size = QueueSize(queue);
+	printf("Queue size after dequeue: %lu\n", size);
+	
+	printf(TITLE "\nFind val \"20\":\n" RESET);	
+	found = QueueFind(queue, &val2, IsMatch);
+	printf("Found value: %d\n", *(int*)found);	
+	
+	printf(TITLE "\nCurrent queue:\n" RESET);	
+	peeked_val = QueuePeek(queue);
+	printf("%d ", *peeked_val);
+	QueueDequeue(queue);
+	peeked_val = QueuePeek(queue);
+	printf("%d \n", *peeked_val);
+	QueueDequeue(queue);
+	
+	QueueDestroy(queue);
 	
 	return (0);
 }
 
 /************************************Functions************************************/
+static int IsMatch(const void* data1, const void* data2)
+{
+	return (*(int*)data1 == *(int*)data2);
+}
