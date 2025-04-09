cba49a664cf21264719b2b0630f5555fa88ad3d8
tom.golzman <tomgolzman@gmail.com>
2025-04-09 14:05:37 +0300

    Push files to branch 'circular_buffer'

--- ds/circular_buffer/src/circular_buffer.c
+++ ds/circular_buffer/src/circular_buffer.c
@@ -1,17 +1,118 @@
 /**
 	Written By: Tom Golzman
 	Date: 08/04/2025
-	Reviewed By: 
+	Reviewed By: Sami
 **/
 
 /************************************includes************************************/
+#include <stddef.h> /* offsetof */
+#include <stdlib.h> /* malloc , size_t */
+#include <assert.h> /* assert */
+#include <stdio.h> /* printf for PrintCharBuffer() */
 
 /************************************define************************************/
-#define TRUE 1
-#define FALSE 0
-#define SUCCESS 0
-#define FAIL 1
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
 /************************************typedef************************************/
+typedef struct circular_buffer{
+	size_t size;
+	size_t read;
+	size_t capacity;
+	char buffer[1];
+} cb_t;
 
 /************************************Functions************************************/
+cb_t* CBCreate(size_t capacity)
+{
+	cb_t* c_buffer = (cb_t*)malloc(offsetof(cb_t, buffer) + capacity);
+	
+	assert(NULL != c_buffer);
+	
+	c_buffer->size = 0;
+	c_buffer->capacity = capacity;
+	c_buffer->read = 0;
+	
+	return (c_buffer);
+}
+
+void CBDestroy(cb_t* cb)
+{
+	assert(NULL != cb);
+	
+	free(cb);
+	cb = NULL;
+}
+
+size_t CBWrite(cb_t* dest, const void* src, size_t n)
+{
+	size_t i = 0;
+	size_t count = 0;
+	size_t write_index = 0;
+	size_t avalible = dest->capacity - dest->size;
+	
+	assert(NULL != src);
+	assert(NULL != dest);
+	
+	n = MIN(avalible, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		write_index = (dest->read + dest->size) % dest->capacity;
+		dest->buffer[write_index] = ((char*)src)[i];
+		dest->size++;
+		count++;
+	}
+	
+	return (count);
+}
+
+size_t CBRead(cb_t* src, void* dest, size_t n)
+{
+	size_t i = 0;
+	size_t count = 0;
+	size_t read_index = 0;
+
+	assert(NULL != src);
+	assert(NULL != dest);	
+	
+	n = MIN(src->size, n);
+
+	for (i = 0; i < n; i++)
+	{
+		read_index = (src->read + i) % src->capacity;
+		((char*)dest)[i] = src->buffer[read_index];
+		count++;
+	}
+	
+	src->read = (src->read + count) % src->capacity;
+	src->size = src->size - count;
+	
+	return (count);
+}
+
+size_t CBGetSize(const cb_t* cb)
+{
+	assert(NULL != cb);
+
+	return (cb->size);
+}
+
+size_t CBGetCapacity(const cb_t* cb)
+{
+	assert(NULL != cb);
+
+	return (cb->capacity);
+}
+
+void PrintCharBuffer(cb_t* cb, size_t size)
+{
+	size_t i = 0;
+
+	assert(NULL != cb);
+	
+	for (i = 0; i < size; i++)
+	{
+		printf("%c ", cb->buffer[i]);
+	}
+	printf("\n");
+}
--- ds/circular_buffer/test/circular_buffer_test.c
+++ ds/circular_buffer/test/circular_buffer_test.c
@@ -9,14 +9,62 @@
 #define BOLD "\033[1m"
 #define TITLE "\033[1;4m"
 
-/************************************Functions Forward Decleration************************************/
-
 /************************************main************************************/
 int main(void)
 {
-	
+cb_t* buff_write = CBCreate(5);
+	char buff_read[5] = {'0', '0', '0', '0', '0'};
+	char input1[] = {'A', 'B', 'C'};
+	char input2[] = {'D', 'E'};
+	char input3[] = {'X', 'Y', 'Z'};
+	size_t written = 0;
+	size_t read = 0;
+	size_t i = 0;
+
+	printf(TITLE "Test: CBWrite Regular\n" RESET);
+	printf("input1[] = A,B,C | n = 3 | buff_write = ");
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));
+	written = CBWrite(buff_write, input1, 3);
+	printf("written: %lu | after write: ", written);
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));
+
+	printf(TITLE "\nTest: CBWrite Complete Fill\n" RESET);
+	printf("input2[] = D,E | n = 2 | buff_write = ");
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));	
+	written = CBWrite(buff_write, input2, 2);
+	printf("written: %lu | after write: ", written);	
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));
+
+
+	printf(TITLE "\nTest: CBRead 3 Bytes\n" RESET);
+	read = CBRead(buff_write, buff_read, 3);
+	printf("read: %lu | data: ", read);
+	for (i = 0; i < read; ++i)
+	{
+		printf("%c ", buff_read[i]);
+	}
+	printf("| after read: ");
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));
+
+
+	printf(TITLE "\nTest: CBWrite Overflow Attempt\n" RESET);
+	printf("input2[] = X,Y,Z | n = 3 | buff_write = ");
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));		
+	written = CBWrite(buff_write, input3, 3);
+	printf("written: %lu | after write: ", written);	
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write)); 
+
+	printf(TITLE "\nTest: CBRead Remaining Bytes\n" RESET);
+	read = CBRead(buff_write, buff_read, 5);
+	printf("read: %lu | data: ", read);
+	for (i = 0; i < read; ++i)
+	{
+		printf("%c ", buff_read[i]);
+	}
+	printf("| after read: ");
+	PrintCharBuffer(buff_write, CBGetCapacity(buff_write));
+
+	CBDestroy(buff_write);
 	
 	return (0);
 }
-
-/************************************Functions************************************/
