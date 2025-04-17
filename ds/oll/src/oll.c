c71af6f7d2e0cc707c6bd6d40cd5b24382c0a8ef
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-04-17 10:09:30 +0300

    Upload files before review

--- ds/oll/src/oll.c
+++ ds/oll/src/oll.c
@@ -5,6 +5,14 @@
 **/
 
 /************************************includes************************************/
+/* in header file */
+/* to check that 2 iterators are of the same list */
+/* also useful to get access to the default comparer function from the iterators in OListFind() */
+
+#include <stdlib.h> /* malloc, free */
+#include <assert.h> /* assert */
+
+#include "oll.h"
 
 /************************************define************************************/
 #define TRUE 1
@@ -12,6 +20,294 @@
 #define SUCCESS 0
 #define FAIL 1
 
-/************************************typedef************************************/
+#ifndef NDEBUG /* debug */
+	#define D_TO_O_ITER_LIST(d_iter, o_list) (DllIterToOllIter(d_iter, o_list))
+	#define D_TO_O_ITER_ITER(d_iter, o_iter) (DllIterToOllIter(d_iter, o_iter.list))	
+#else /* release */
+	#define D_TO_O_ITER_LIST(d_iter, o_list) (DllIterToOllIter(d_iter))
+	#define D_TO_O_ITER_ITER(d_iter, o_iter) (DllIterToOllIter(d_iter))
+#endif
 
-/************************************Functions************************************/
+#define O_TO_D_ITER(o_iter) (OllIterToDllIter(o_iter))
+
+/************************************typedef************************************/
+struct ordered_list {
+    olist_comparer_t comparer; 	/* a function used to compare the values within it */
+    void* comparer_param;
+    dll_t* list; 				/* a dll so we would be able to use dll functions */
+};
+
+/************************************Private Functions************************************/
+#ifndef NDEBUG /* debug */
+	static oll_iter_t DllIterToOllIter(iter_t iter, const oll_t* list)
+	{
+		oll_iter_t oll_iter;
+
+		oll_iter.iter = iter;
+		oll_iter.list = list;
+		
+		return (oll_iter);
+	}
+#else /* release */
+	static oll_iter_t DllIterToOllIter(iter_t iter)
+	{
+		oll_iter_t oll_iter;
+
+		oll_iter.iter = iter;
+		
+		return (oll_iter);
+	}
+#endif
+
+static iter_t OllIterToDllIter(oll_iter_t iter)
+{
+	return (iter.iter);
+}
+
+static int IsIterInSameList(oll_iter_t iter1, oll_iter_t iter2)
+{
+	return (iter1.list == iter2.list);
+}
+
+/************************************Public Functions************************************/
+oll_t* OListCreate(olist_comparer_t comparer, void* param)
+{
+	oll_t* oll = NULL;
+	dll_t* dll = NULL;
+	
+	assert(NULL != comparer);
+	
+	oll = (oll_t*)malloc(sizeof(oll_t));
+	if (NULL == oll)
+	{
+		return (NULL);
+	}
+	
+	dll = DListCreate();
+	if (NULL == dll)
+	{
+		free(oll);
+		oll = NULL;
+		
+		return (NULL);
+	}
+	
+	oll->comparer = comparer;
+	oll->comparer_param = param;
+	oll->list = dll;
+	
+	return (oll);
+}
+
+void OListDestroy(oll_t* list)
+{
+	DListDestroy(list->list);
+	
+	free(list);
+	list = NULL;
+}
+
+oll_iter_t OListInsert(oll_t* list, void* data)
+{
+	iter_t curr = NULL;
+	iter_t end = NULL;
+	iter_t new_node = NULL;
+
+	assert(NULL != list);
+	
+	curr = DListBegin(list->list);
+	end = DListEnd(list->list);
+	
+	while (!DListIsSameIter(curr, end) && list->comparer(DListGetData(curr), data, list->comparer_param) < 0)
+	{
+		curr = DListNext(curr);
+	}
+	
+	new_node = DListInsert(list->list, curr, data);
+
+	return (D_TO_O_ITER_LIST(new_node, list));
+}
+
+oll_iter_t OListRemove(oll_iter_t iter)
+{
+	iter_t result = DListRemove(O_TO_D_ITER(iter));
+	
+	return (D_TO_O_ITER_ITER(result, iter));
+}
+
+void OListMerge(oll_t* dest, oll_t* src)
+{
+	iter_t dest_iter = NULL;
+	iter_t dest_end = NULL;
+	iter_t src_iter = NULL;
+	iter_t src_end = NULL;
+	iter_t next_src = NULL;
+	
+	assert(NULL != dest);
+	assert(NULL != src);
+	
+	dest_iter = DListBegin(dest->list);
+	dest_end = DListEnd(dest->list);
+	src_iter = DListBegin(src->list);
+	src_end = DListEnd(src->list);
+	
+	while (!DListIsSameIter(src_iter, src_end))
+	{
+		next_src = DListNext(src_iter);
+		
+		while (!DListIsSameIter(dest_iter, dest_end) && dest->comparer(DListGetData(dest_iter), DListGetData(src_iter), dest->comparer_param) < 0)
+		{
+			dest_iter = DListNext(dest_iter);
+		}
+		
+		DListSplice(dest_iter, src_iter, next_src);
+		src_iter = next_src;
+	}
+}
+
+size_t OListSize(const oll_t* list)
+{
+	dll_t* dll = NULL;
+
+	assert(NULL != list);
+	
+	dll = list->list;
+	
+	return (DListSize(dll));
+}
+
+int OListIsEmpty(const oll_t* list)
+{
+	assert(NULL != list);
+	
+	return (DListIsEmpty(list->list));
+}
+
+oll_iter_t OListBegin(const oll_t* list)
+{
+	assert(NULL != list);
+	
+	return (D_TO_O_ITER_LIST(DListBegin(list->list), list));
+}
+
+oll_iter_t OListEnd(const oll_t* list)
+{
+	dll_t* dll = NULL;
+
+	assert(NULL != list);
+	
+	dll = list->list;
+	
+	return (D_TO_O_ITER_LIST(DListEnd(dll), list));
+}
+
+oll_iter_t OListNext(oll_iter_t iter)
+{
+	iter_t dll_iter = O_TO_D_ITER(iter);
+	
+	return (D_TO_O_ITER_ITER(DListNext(dll_iter), iter));
+}
+
+oll_iter_t OListPrev(oll_iter_t iter)
+{
+	iter_t dll_iter = O_TO_D_ITER(iter);
+
+	return (D_TO_O_ITER_ITER(DListPrev(dll_iter), iter));
+}
+
+void OListForEach(oll_iter_t from ,oll_iter_t to, olist_action_t action, void* param)
+{
+	iter_t dll_iter_from = O_TO_D_ITER(from);
+	iter_t dll_iter_to = O_TO_D_ITER(to);
+	
+	assert(NULL != action);
+	
+	DListForEach(dll_iter_from, dll_iter_to, action, param);
+}
+
+oll_iter_t OListFind(const oll_t* list, oll_iter_t from, oll_iter_t to, const void* data_to_find)
+{
+	iter_t dll_iter_from = O_TO_D_ITER(from);
+	iter_t dll_iter_to = O_TO_D_ITER(to);
+	iter_t curr = dll_iter_from;
+
+	while (!DListIsSameIter(curr, dll_iter_to))
+	{
+		if (0 == list->comparer(DListGetData(curr), data_to_find, list->comparer_param))
+		{
+			return (D_TO_O_ITER_LIST(curr, list));
+		}
+		curr = DListNext(curr);
+	}
+	
+	return (to);
+}
+
+oll_iter_t OListFindIf(oll_iter_t from, oll_iter_t to, olist_is_match_t is_match, const void* data_to_find)
+{
+	iter_t dll_iter_from = O_TO_D_ITER(from);
+	iter_t dll_iter_to = O_TO_D_ITER(to);
+	iter_t curr = dll_iter_from;
+
+	assert(NULL != is_match);
+	
+	while (!DListIsSameIter(curr, dll_iter_to))
+	{
+		if (is_match(DListGetData(curr), data_to_find))
+		{
+			return (D_TO_O_ITER_LIST(curr, from.list));
+		}
+		curr = DListNext(curr);
+	}
+	
+	return (to);
+}
+
+void* OListGetData(oll_iter_t iter)
+{
+	iter_t dll_iter = O_TO_D_ITER(iter);
+
+	return (DListGetData(dll_iter));
+}
+
+int OListIsSameIter(oll_iter_t iter1, oll_iter_t iter2)
+{
+	iter_t dll_iter1 = O_TO_D_ITER(iter1);
+	iter_t dll_iter2 = O_TO_D_ITER(iter2);
+
+	return (DListIsSameIter(dll_iter1, dll_iter2));
+}
+
+void OListPopFront(oll_t* list)
+{
+	dll_t* dll = NULL;
+
+	assert(NULL != list);
+	
+	dll = list->list;
+	
+	DListPopFront(dll);
+}
+
+void OListPopBack(oll_t* list)
+{
+	dll_t* dll = NULL;
+
+	assert(NULL != list);
+	
+	dll = list->list;
+	
+	DListPopBack(dll);
+}
+
+
+
+
+
+
+
+
+
+
+
+
