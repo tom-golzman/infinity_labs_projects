9c063ec14defc03382514d316a5bb4aaebcd331d
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-06-04 14:38:10 +0300

    Push new branch 'set'

---
+++ ds/set/test/test_set.c
@@ -0,0 +1,33 @@
+/************************************includes************************************/
+#include <stdio.h> /* printf */
+
+#include "set.h"
+
+/************************************define************************************/
+#define GREEN "\033[1;32m"
+#define RED "\033[1;31m"
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define BOLD_TITLE "\033[1;4m"
+#define TITLE "\033[4m"
+
+#define TRUE	(1)
+#define FALSE	(0)
+#define SUCCESS (0)
+#define FAIL	(1)
+
+/************************************Functions Forward Decleration************************************/
+
+/************************************main************************************/
+int main(void)
+{
+	
+	
+	printf("\n");
+	
+	return (0);
+}
+
+/************************************Functions************************************/
+
+    printf(TITLE "\nTest: ()\n" RESET);
