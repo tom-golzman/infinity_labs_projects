214ba3d1436a6793ad1821470e040803cf538d35
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-06-04 17:50:53 +0300

    Pseudo code

--- ds/set/include/set.h
+++ ds/set/include/set.h
@@ -6,23 +6,16 @@
 /************************************typedef************************************/
 typedef struct set set_t;
 
-/**
-	@RETURN: status. TRUE or FALSE.
-**/
-typedef int (*set_is_match_t)(const void* current_data, const void* data_to_compare, void* param);
+typedef int (*set_is_same_key_t)(const void* key1, const void* key2, const void* param);
 
 /**
-	@RETURN: index in the hash table.
+	@RETURN: number in the range 0 to at least the requested size of the set (can be bigger).
 **/
-typedef size_t (*hash_func_t)(const void* data);
+typedef size_t (*hash_func_t)(const void* key, const void* param);
 
-/**
-	@RETURN: status. SUCCESS or FAIL.
-**/
-typedef int (*set_action_func_t)(void* data, void* param);
 
 /************************************Functions************************************/
-set_t* SetCreate(const set_is_match_t is_match, void* param, const hash_func_t hash_func, size_t table_size);
+set_t* SetCreate(size_t capacity, set_is_same_key_t is_same_key, const void* is_same_param, hash_func_t hash_func, const void* hash_func_param);
 
 /**
 	@NOTES: the set can be NULL.
@@ -31,31 +24,36 @@ void SetDestroy(set_t*);
 
 /**
 	@RETURN: returns the data that has been removed.
+	@LIMITATIONS: undefined to remove a key that does not exist.
 **/
-void* SetRemove(set_t*, const void* data, const set_is_match_t is_match, void* param);
+void* SetRemove(set_t*, const void* key);
 
 /**
 	@RETURN: status. allocation can fail.
 **/
-int SetInsert(set_t*, const void* data, const set_is_match_t is_match, void* param);
+int SetInsert(set_t*, const void* key);
 
 /**
-	@COMPLEXITY: O(size).
+	@COMPLEXITY: ?
 **/
 size_t SetSize(const set_t*);
 
-/**
-	@RETURN: status. TRUE or FALSE.
-**/
 int SetIsEmpty(const set_t*);
 
 /**
-	@RETURN: the data that was found. NULL if wasn't found
+	@RETURN: NULL if wasn't found
 **/
 void* SetFind(const set_t*, const void* data);
 
 /**
-	@RETURN: status. SUCCESS or FAIL.
+	@RETURN: status
+	@NOTES: undefined if it changes the key part of the object
+**/
+typedef int (*set_action_func_t)(void* data, const void* param);
+
+/**
+	@RETURN: last status returned by set_action_func_t.
+	@NOTES: stops and returnes if set_action_func_t fails.
 **/
 int SetForEach(set_t*, set_action_func_t, void* param);
 
--- ds/set/src/set.c
+++ ds/set/src/set.c
@@ -16,5 +16,69 @@
 #define FAIL 	(1)
 
 /************************************typedef************************************/
-
+/* struct set */
+	/* capacity */
+	/* dll_t* table */
+	/* size */
+		
 /************************************Functions************************************/
+set_t* SetCreate(size_t capacity, set_is_same_key_t is_same_key, const void* is_same_param, hash_func_t hash_func, const void* hash_func_param)
+{
+	/* allocate memory for the set */
+	/* initialize array elements with an empty dll */
+	/* initialize the functions (is_same_key & hash_func) and the params */
+}
+
+void SetDestroy(set_t*)
+{
+	/* destroy all the keys in the set */
+	/* free the memory of the set */
+}
+
+void* SetRemove(set_t*, const void* key)
+{
+	/* calculate the hash value of the key */
+	/* find the hash value to remove in the hash table */
+	/* remove the key from the hash table and free its memory */
+}
+
+int SetInsert(set_t*, const void* key)
+{
+	/* calculate the hash value of the key */
+	/* push back the hash value in the place and increase the size */
+}
+
+size_t SetSize(const set_t*)
+{
+	/* returns the size field from the struct */
+}
+
+int SetIsEmpty(const set_t*)
+{
+	/* check if the size field in the struct is zero or not */
+}
+
+void* SetFind(const set_t*, const void* data)
+{
+	/* calculate the hash value of the key */
+	/* go to the hash value in the hash table and get the data */
+}
+
+int SetForEach(set_t*, set_action_func_t, void* param)
+{
+	/* iterate all the hash table and run the action_func on the data */
+	/* on first fail return the status */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
