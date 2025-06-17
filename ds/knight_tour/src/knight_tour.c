385139d52d296bf192b118a2ac9ba33020dc695d
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-06-17 13:19:19 +0300

    knight tour before review

--- ds/knight_tour/src/knight_tour.c
+++ ds/knight_tour/src/knight_tour.c
@@ -23,65 +23,130 @@ typedef struct position {
 } position_t;
 
 /**************************Private Functions Decleration**************************/
-static int KnigtTourStep(board_t board, position_t position);
-static int PositionToIndex(position_t position);
-static int IsOutOfBorad(position_t position);
-static position_t GetNextMove(position_t curr_position, int direction);
-static int IsVisited(board_t board, position_t position);
+static int KnightTourStep(board_t board, position_t position);
+static size_t PositionToIndex(position_t position);
+static int IsOutOfBoard(position_t position);
+static position_t GetNextPosition(position_t curr_position, int direction);
+static int IsVisitedPosition(board_t board, position_t position);
 
 /************************************Functions************************************/
 int IsKnightTourPathFound(int x, int y)
 {
-	board_t board;
+	/* create board */
+	board_t board = BitArrInit(OFF);
 	
+	/* create position struct */
+	position_t position;	
+		
 	/* assert */
 	assert(x >= 0);
-	assert(x < BORAD_SIZE);
+	assert(x < BOARD_SIZE);
 	assert(y >= 0);
-	assert(y < BORAD_SIZE);
+	assert(y < BOARD_SIZE);
 	
-	/* create borad */
+	/* intialize position with given x and y */
+	position.x = x;
+	position.y = y;
 	
 	/* ?? mark start position as visited ?? */
 	
 	/* call the recursive function */
+	return KnightTourStep(board, position);
 }
 
 /********************************Private Functions********************************/
-static int KnigtTourStep(board_t board, position_t position)
+static int KnightTourStep(board_t board, position_t position)
 {
-	/* assert */
-	
-	/* trivial case - out of the borad */
-		/* return false */
-	/* trivial case - already visited position */
-		/* return false */
+	position_t new_position;
+	size_t position_idx = 0;
+	int status = FALSE;
+	int i = 0;
 
+	/* trivial case - out of the borad */
+	if (IsOutOfBoard(position))
+	{
+		/* return false */
+		return FALSE;		
+	}
+	/* trivial case - already visited position */
+	if (IsVisitedPosition(board, position))
+	{
+		/* return false */
+		return FALSE;
+	}
+	
+	/* convert position to index */
+	position_idx = PositionToIndex(position);
+	
 	/* mark position as visited */
+	board = BitArrSetVal(board, position_idx, ON);
 	
 	/* trivial case - borad is full */
-		/* return success */
-		
+	if (0 == BitArrCountOff(board))
+	{
+		/* return true */
+		return TRUE;
+	}	
 
 	/* foreach direction (8 directions) */
+	for (i = 0; i < NUM_MOVES; ++i)
+	{
 		/* calculate next position from current position */
+		new_position = GetNextPosition(position, i);
+		
 		/* call the recursive function with next position */
-		/* if returned success */
-			/* return success */
-			
-	/* return failure */ 
-}
-
-static int PositionToIndex(position_t position)
-{
+		status = KnightTourStep(board, new_position);
+		
+		/* if returned true - return it */
+		if (TRUE == status)
+		{
+			return TRUE;
+		}
+	}
 	
+	/* return false */
+	return FALSE; 
 }
 
-static int IsOutOfBorad(position_t position)
-{}
+static size_t PositionToIndex(position_t position)
+{
+	return (size_t)(position.x * BOARD_SIZE + position.y);
+}
 
-static position_t GetNextMove(position_t curr_position, int direction)
-{}
+static int IsOutOfBoard(position_t position)
+{
+	return (position.x < 0) || (position.x >= BOARD_SIZE) ||
+		   (position.y < 0) || (position.y >= BOARD_SIZE);
+}
 
-static int IsVisited(board_t board, position_t position)
-{}
+static position_t GetNextPosition(position_t curr_position, int direction)
+{
+	position_t next_position;
+	
+	/* intialize moves LUT (static - intialize once, const - can't be modified */
+	static const int moves[NUM_MOVES][2] = { { 1,2} , { 1,-2} , { 2,1} , { 2,-1} ,
+											 {-1,2} , {-1,-2} , {-2,1} , {-2,-1} };
+	
+	/* assert */
+	assert(direction >= 0);
+	assert(direction < NUM_MOVES);
+	
+	/* initialize next position */
+	next_position.x = curr_position.x + moves[direction][0];
+	next_position.y = curr_position.y + moves[direction][1];
+	
+	/* return next poisition */
+	return next_position;
+}
+
+static int IsVisitedPosition(board_t board, position_t position)
+{
+	size_t position_idx = PositionToIndex(position);
+	
+	if (ON == BitArrGetVal(board, position_idx))
+	{
+		return TRUE;
+	}
+	
+	return FALSE;
+}
--- ds/knight_tour/test/test_knight_tour.c
+++ ds/knight_tour/test/test_knight_tour.c
@@ -7,26 +7,55 @@
 /************************************define************************************/
 
 /************************************Functions Forward Decleration************************************/
+void TestKnightTour();
 
 /************************************main************************************/
 int main(void)
 {
-	    printf(BOLD_TITLE "\nTest: ()\n" RESET);
-	
+	TestKnightTour();
+		
 	printf("\n");
 	
 	return (0);
 }
 
-	if ()
+/************************************Functions************************************/
+void TestKnightTour()
+{
+	int result = 0;
+
+	printf(BOLD_TITLE "\nTest: IsKnightTourPathFound()\n" RESET);
+
+	/* TEST 1: left up edge (0,0) */
+	result = IsKnightTourPathFound(0, 0);
+	if (TRUE == result)
 	{
-		printf(GREEN "Test 1 PASSED!\n" RESET);
+		printf(GREEN "TEST 1 PASSED\n" RESET);
 	}
 	else
 	{
-		printf(RED "Test 1 FAILED!\n" RESET);
+		printf(RED "TEST 1 FAILED: " RESET "Expected TRUE at (0,0)\n");
 	}
 
-/************************************Functions************************************/
+	/* TEST 2: board center (3,3) */
+	result = IsKnightTourPathFound(3, 3);
+	if (TRUE == result)
+	{
+		printf(GREEN "TEST 2 PASSED\n" RESET);
+	}
+	else
+	{
+		printf(RED "TEST 2 FAILED: " RESET "Expected TRUE at (3,3)\n");
+	}
 
-
+	/* TEST 3: right down edge (7,7) */
+	result = IsKnightTourPathFound(7, 7);
+	if (TRUE == result)
+	{
+		printf(GREEN "TEST 3 PASSED\n" RESET);
+	}
+	else
+	{
+		printf(RED "TEST 3 FAILED: " RESET "Expected TRUE at (7,7)\n");
+	}
+}
