ba992303443fbac8d629a2e8b76fb3b5d8e7cf79
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-06-19 08:46:08 +0300

    simple_shell before review

---
+++ system_programming/simple_shell/include/simple_shell.h
@@ -0,0 +1,7 @@
+#ifndef __ILRD_SIMPLE_SHELL_H__
+#define __ILRD_SIMPLE_SHELL_H__
+
+/************************************Functions************************************/
+void RunShell();
+
+#endif /*__ILRD_SIMPLE_SHELL_H__*/
