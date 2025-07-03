4a30e6b191e1e7cea182b37ec15a918719bd9cdf
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-07-03 09:06:30 +0300

    exercises 1 & 2 tested - producers consumers

---
+++ system_programming/producers_consumers/producers_consumers1.c
@@ -0,0 +1,120 @@
+/**
+	Written By: Tom Golzman
+	Date: 01/07/2025
+	Reviewed By: 
+**/
+
+/************************************includes*************************************/
+#include <assert.h>		/* assert() */
+#include <pthread.h>	/* pthread_t */
+#include <stdio.h>		/* printf() */
+
+#include "utils.h"		/* SUCCESS, FAIL, TRUE, FALSE, DEBUG_ONLY(), BAD_MEM(), ExitIfBad() */
+
+/**************************************define*************************************/
+enum { CONSUMED = 0, PRODUCED = 1 };
+
+volatile int shared_val = 0;
+volatile int is_val_available = 0;
+
+/********************************Private Functions********************************/
+static void* ProduceThreadFunc(void* arg);
+static void* ConsumeThreadFunc(void* arg);
+static int Producer();
+static void Consumer(int val);
+
+/************************************Functions************************************/
+int main()
+{
+	int status = 0;
+	
+	/* create threads for producer and consumer */
+	pthread_t producer;
+	pthread_t consumer;
+	
+	status = pthread_create(&producer, NULL, ProduceThreadFunc, NULL);
+	ExitIfBad(SUCCESS == status, FAIL, "pthread_create() producer FAILED!");
+	
+	status = pthread_create(&consumer, NULL, ConsumeThreadFunc, NULL);
+	ExitIfBad(SUCCESS == status, FAIL, "pthread_create() consumer FAILED!");
+
+	while (1)
+	{
+		
+	}
+
+	/* return SUCCESS */
+	return SUCCESS;
+}
+
+static void* ProduceThreadFunc(void* arg)
+{
+	int val = 0;
+		
+	while (1)
+	{
+		/* call Producer() */
+		val = Producer();
+
+		/* atomic function - while the value wasn't read */
+		while (CONSUMED != __atomic_load_n(&is_val_available, __ATOMIC_SEQ_CST))
+		{
+			/* busy wait */
+		}
+			
+		/* write the shared value */
+		shared_val = val;
+		
+		/* atomic function - update that the value was written */
+		__atomic_store_n(&is_val_available, PRODUCED, __ATOMIC_SEQ_CST);
+	}
+	
+	return NULL;
+	
+	(void)arg;
+}
+
+static int Producer()
+{
+	static int val = 10;
+	
+	printf("produced: %d\n", val);
+	
+	++val;
+	
+	/* return the value */
+	return val;
+}
+
+static void* ConsumeThreadFunc(void* arg)
+{
+	int val = 0;
+	
+	while (1)
+	{
+		/* atomic function - while the value wasn't written */
+		while (PRODUCED != __atomic_load_n(&is_val_available, __ATOMIC_SEQ_CST))	
+		{
+			/* busy wait */
+		}
+			
+		/* copy the shared value */
+		val = shared_val;
+		
+		/* atomic function - update that the value was read */
+		__atomic_store_n(&is_val_available, CONSUMED, __ATOMIC_SEQ_CST);
+		
+		/* call Consumer() */
+		Consumer(val);
+	}
+	
+	return NULL;
+	
+	(void)arg;
+}
+
+static void Consumer(int val)
+{
+	/* print the value */
+	printf("consumer: %d\n", val);
+}
---
+++ system_programming/producers_consumers/producers_consumers3.c
@@ -0,0 +1,64 @@
+int main()
+{
+	/* create producers & consumers threads arrays */
+
+	/* create a linked list */
+	/* handle failure */
+		
+	/* create a semaphore */
+	
+	/* for each index in producers array */
+		/* alloacte data */
+		/* handle failure */
+		/* create a thread */		
+		/* handle failure */
+	
+	/* for each index in consumers array */
+		/* alloacte data */
+		/* handle failure */
+		/* create a thread */		
+		/* handle failure */
+	
+	/* return SUCCESS */
+}
+
+static void* ProduceThreadFunc(void* arg)
+{
+	/* while 1 */
+		/* call Producer() */
+		
+		/* lock the mutex */
+		/* push the value to the list */
+		/* increment the semaphore by 1 with sem_post() */
+		/* unlock the mutex */
+	/* end while */
+	
+	/* return NULL */
+}
+
+static int Producer()
+{
+	/* return the value */
+}
+
+static void* ConsumeThreadFunc(void* arg)
+{
+	/* while 1 */
+		/* wait to new message */
+		/* lock the mutex */
+
+		/* pop the value from the list */
+		/* free the value */
+		
+		/* unlock the mutex */
+		/* call Consumer() */
+		
+	/* end while */
+	
+	/* return NULL */
+}
+
+static void Consumer()
+{
+	/* print the value */
+}
