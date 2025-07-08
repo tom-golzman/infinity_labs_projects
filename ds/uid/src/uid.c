40ead012f55e7ec43631b6e0cde865ce5019c8fc
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-07-08 17:21:45 +0300

    updated uid. now the scheduler supports multi threading

--- ds/uid/src/uid.c
+++ ds/uid/src/uid.c
@@ -7,23 +7,22 @@
 /************************************includes************************************/
 #define _POSIX_C_SOURCE 200112L /* for gethostname() warning */
 #include <unistd.h> 	/* gethostname() */
-
 #include <netdb.h> 		/* gethostbyname() */
 #include <netinet/in.h> /* struct in_addr */
 
 #include "uid.h"
 
 /************************************define************************************/
-#define TRUE (1)
-#define FALSE (0)
+#define TRUE	(1)
+#define FALSE	(0)
 
-#define INVALID_IP ((unsigned int)0)
-#define INVALID_PID ((pid_t)0)
-#define INVALID_ID ((unsigned long)0)
+#define INVALID_IP	 ((unsigned int)0)
+#define INVALID_PID	 ((pid_t)0)
+#define INVALID_ID	 ((unsigned long)0)
 #define INVALID_TIME ((time_t)-1)
 
 /************************************Global Variables************************************/
-static unsigned long counter = 0;
+static volatile unsigned long counter = 0;
 
 const ilrd_uid_t invalid_uid = {INVALID_IP, INVALID_PID, INVALID_ID, INVALID_TIME};
 
@@ -35,7 +34,6 @@ ilrd_uid_t UIDCreate()
 {
 	unsigned int ip = getIp();
 	pid_t pid = getpid();
-	unsigned long id = counter + 1;
 	time_t curr_time = time(NULL);
 	ilrd_uid_t uid;
 
@@ -46,14 +44,8 @@ ilrd_uid_t UIDCreate()
 	
 	uid.ip = ip;
 	uid.pid = pid;
-	uid.id = id;
+	uid.id = __atomic_fetch_add(&counter, 1, __ATOMIC_SEQ_CST);
 	uid.time = curr_time;
-	
-	counter++;
-	if (0 == counter)
-	{
-		counter++;
-	}
 	
 	return (uid);
 }
@@ -75,13 +67,13 @@ static unsigned int getIp()
 	struct hostent* host = NULL;
 	if (-1 == gethostname(hostname, sizeof(hostname)))
 	{
-		return (0);
+		return (INVALID_IP);
 	}
 
 	host = gethostbyname(hostname);
 	if (NULL == host)
 	{	
-		return (0);
+		return (INVALID_IP);
 	}
 
 	return (((struct in_addr*)host->h_addr_list[0])->s_addr);
