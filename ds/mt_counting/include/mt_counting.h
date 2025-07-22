b497ba2434e27d08ba5731edf639e45bd06f3080
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-07-22 08:55:09 +0300

    mt_counting tested

--- ds/mt_counting/include/mt_counting.h
+++ ds/mt_counting/include/mt_counting.h
@@ -1,13 +1,7 @@
 #ifndef __ILRD_MT_COUNTING_H__
 #define __ILRD_MT_COUNTING_H__
 
-/************************************includes************************************/
-
-/************************************typedef************************************/
-
-/************************************define************************************/
-
 /************************************Functions************************************/
-int MTCounting(const char* file_path);
+int MTCounting(const char* file_path, int num_threads);
 
 #endif /*__ILRD_MT_COUNTING_H__*/
--- ds/mt_counting/test/test_mt_counting.c
+++ ds/mt_counting/test/test_mt_counting.c
@@ -1,32 +1,55 @@
 /************************************includes************************************/
-#include <stdio.h> /* printf */
+#include <stdio.h>		/* printf */
+#include <sys/time.h>	/* struct timeva, gettimeofday() */
 
 #include "test_utils.h" /* colors, titles, status, boolean */
 #include "mt_counting.h"
 
 /************************************define************************************/
-
-/************************************Functions Forward Decleration************************************/
+enum
+{
+	NUM_THREADS = 20
+};
 
 /************************************main************************************/
 int main(void)
 {
-	    printf(BOLD_TITLE "\nTest: ()\n" RESET);
+	int result = 0;
+	int i = 1;
+	struct timeval start, end;
+	double elapsed_sec = 0;
+	double min_time = 10000;
+	int best_num_threads = -1;
+	
+	printf(BOLD_TITLE "\nTest: ()\n" RESET);
+	
+	for (i = 1; i <= NUM_THREADS; ++i)
+	{
+		gettimeofday(&start, NULL);
+		
+		result = MTCounting("/home/tom/git/ds/mt_counting/linux_dict_10x.txt", i);	
+		
+		gettimeofday(&end, NULL);
+		
+		elapsed_sec = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+				
+		if (SUCCESS != result)
+		{
+			printf(RED "Test with %d threads FAILED!\n" RESET, i);
+		}
+		
+		if (elapsed_sec < min_time)
+		{
+			min_time = elapsed_sec;
+			best_num_threads = i;
+		}
+		
+		printf(GREEN "%d threads -> %.5f sec\n\n" RESET, i, elapsed_sec);
+	}
+	
+	printf(BOLD "ideal num threads: %d (time: %.5f)\n" RESET, best_num_threads, min_time);
 	
 	printf("\n");
 	
 	return (0);
 }
-
-	if ()
-	{
-		printf(GREEN "Test 1 PASSED!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 1 FAILED!\n" RESET);
-	}
-
-/************************************Functions************************************/
-
-
