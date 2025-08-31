5dc58bd1598771e283c851df2a3eb2cbeb593bd6
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-08-31 19:56:35 +0300

    scheduler test updated

--- ds/scheduler/test/scheduler_test.c
+++ ds/scheduler/test/scheduler_test.c
@@ -1,19 +1,11 @@
-/************************************includes************************************/
 #define _POSIX_C_SOURCE 200112L
 
-#include <stdio.h> /* printf */
+#include <stdio.h>
+#include <unistd.h>
 
 #include "scheduler.h"
+#include "test_utils.h"
 
-/************************************define************************************/
-#define GREEN "\033[1;32m"
-#define RED "\033[1;31m"
-#define RESET "\033[0m"
-#define BOLD "\033[1m"
-#define TITLE "\033[1;4m"
-
-#define TRUE (1)
-#define FALSE (0)
 #define TO_RESCHEDULE (1)
 #define DONT_RESCHEDULE (0)
 
@@ -22,7 +14,6 @@ typedef struct {
 	sched_t* s;
 } stop_params_t;
 
-/************************************Functions Forward Decleration************************************/
 void TestSchedCreateAndDestroy();
 void TestSchedAddTask();
 void TestSchedRemoveTask();
@@ -48,7 +39,6 @@ static size_t cleanup_counter = 0;
 static int reschedule_counter = 0;
 static ilrd_uid_t target_uid;
 
-/************************************main************************************/
 int main(void)
 {
 	TestSchedCreateAndDestroy();
@@ -59,28 +49,16 @@ int main(void)
 	TestSchedRunAndClear();
 	TestSchedRunAndStop();
 	TestStopWithReschedule();
-	
-    printf("\n");
-    	
-	return (0);
+
+	printf("\n");
+	return 0;
 }
 
-/************************************Functions************************************/
 void TestSchedCreateAndDestroy()
 {
 	sched_t* s = SchedCreate();
-	
-	printf(TITLE "Test: SchedCreate() and SchedDestroy()\n" RESET);
-	
-	if (NULL != s)
-	{
-		printf(GREEN "Test 1 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 1 Failed!\n" RESET);
-	}
-	
+	TITLE("Test: SchedCreate() and SchedDestroy()");
+	RUN_TEST("SchedCreate returns non-null", (NULL != s));
 	SchedDestroy(s);
 }
 
@@ -89,20 +67,11 @@ void TestSchedAddTask()
 	sched_t* s = SchedCreate();
 	time_t now = time(NULL);
 
-	printf(TITLE "\nTest: SchedAddTask()\n" RESET);
-
+	TITLE("Test: SchedAddTask()");
 	SchedAddTask(s, now, ActionGetSize, s, GenericCleanup, NULL, 1);
 	SchedAddTask(s, now + 1, ActionIsEmpty, s, GenericCleanup, NULL, 1);
 
-	if (2 == SchedSize(s))
-	{
-		printf(GREEN "Test 1 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 1 Failed!\n" RESET);
-	}
-
+	RUN_TEST("SchedSize == 2 after 2 inserts", (2 == SchedSize(s)));
 	SchedDestroy(s);
 }
 
@@ -112,24 +81,14 @@ void TestSchedRemoveTask()
 	time_t now = time(NULL);
 	ilrd_uid_t uid1;
 
-	printf(TITLE "\nTest: SchedRemoveTask()\n" RESET);
-
+	TITLE("Test: SchedRemoveTask()");
 	uid1 = SchedAddTask(s, now, ActionGetSize, s, GenericCleanup, NULL, 1);
-	
 	SchedRemoveTask(s, uid1);
 
-	if (TRUE == SchedIsEmpty(s))
-	{
-		printf(GREEN "Test 1 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 1 Failed!\n" RESET);
-	}
-	
-	SchedRemoveTask(s, uid1);
+	RUN_TEST("IsEmpty after RemoveTask", TRUE == SchedIsEmpty(s));
 
-	printf(GREEN "Test 2 Passed!\n" RESET);
+	SchedRemoveTask(s, uid1);
+	RUN_TEST("RemoveTask again does not crash", TRUE);
 
 	SchedDestroy(s);
 }
@@ -139,27 +98,11 @@ void TestSchedIsEmpty()
 	sched_t* s = SchedCreate();
 	time_t now = time(NULL);
 
-	printf(TITLE "\nTest: SchedIsEmpty()\n" RESET);
-
-	if (TRUE == SchedIsEmpty(s))
-	{
-		printf(GREEN "Test 1 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 1 Failed!\n" RESET);
-	}
+	TITLE("Test: SchedIsEmpty()");
+	RUN_TEST("IsEmpty after creation", TRUE == SchedIsEmpty(s));
 
 	SchedAddTask(s, now, ActionGetSize, s, GenericCleanup, NULL, 1);
-
-	if (FALSE == SchedIsEmpty(s))
-	{
-		printf(GREEN "Test 2 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 2 Failed!\n" RESET);
-	}
+	RUN_TEST("IsEmpty == FALSE after insert", FALSE == SchedIsEmpty(s));
 
 	SchedDestroy(s);
 }
@@ -169,257 +112,167 @@ void TestSchedSize()
 	sched_t* s = SchedCreate();
 	time_t now = time(NULL);
 
-	printf(TITLE "\nTest: SchedSize()\n" RESET);
-
-	if (0 == SchedSize(s))
-	{
-		printf(GREEN "Test 1 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 1 Failed!\n" RESET);
-	}
+	TITLE("Test: SchedSize()");
+	RUN_TEST("SchedSize == 0 after creation", 0 == SchedSize(s));
 
 	SchedAddTask(s, now, ActionGetSize, s, GenericCleanup, NULL, 1);
 	SchedAddTask(s, now, ActionGetSize, s, GenericCleanup, NULL, 1);
 
-	if (2 == SchedSize(s))
-	{
-		printf(GREEN "Test 2 Passed!\n" RESET);
-	}
-	else
-	{
-		printf(RED "Test 2 Failed!\n" RESET);
-	}
+	RUN_TEST("SchedSize == 2 after 2 inserts", 2 == SchedSize(s));
 
 	SchedDestroy(s);
 }
 
 void TestSchedRunAndClear()
 {
-    sched_t* s = SchedCreate();
-    time_t now = time(NULL);
-    status_t result;
+	sched_t* s = SchedCreate();
+	time_t now = time(NULL);
+	status_t result;
 
-	printf(TITLE "\nTest: SchedRun() and SchedClear()\n" RESET);
+	TITLE("Test: SchedRun() and SchedClear()");
+	SchedAddTask(s, now + 1, ActionGetSize, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 2, ActionIsEmpty, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 3, ActionPrintSizeBeforeClear, s, GenericCleanup, NULL, 1);
+	target_uid = SchedAddTask(s, now + 4, ActionRemoveSelf, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 5, ActionCreateScheduler, NULL, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 6, ActionClearAllWithPrint, s, GenericCleanup, NULL, 1);
 
-    SchedAddTask(s, now + 1, ActionGetSize, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 2, ActionIsEmpty, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 3, ActionPrintSizeBeforeClear, s, GenericCleanup, NULL, 1);
-    target_uid = SchedAddTask(s, now + 4, ActionRemoveSelf, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 5, ActionCreateScheduler, NULL, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 6, ActionClearAllWithPrint, s, GenericCleanup, NULL, 1);	
+	result = SchedRun(s);
+	sleep(5);
 
-    result = SchedRun(s);
-    sleep(5);
-        
-    if (STOPPED_DEFAULT == result && cleanup_counter >= 5)
-    {
-        printf(GREEN "Test 1 Passed!\n" RESET);
-    }
-    else
-    {
-        printf(RED "Test 1 Failed!\n" RESET);
-        printf("reschedule_counter = %d\n", reschedule_counter);
-		printf("cleanup_counter = %lu\n", cleanup_counter);        
-        printf("result = %d\n", result);
-    }
-    
-    SchedDestroy(s);
+	RUN_TEST("Run-Clear scenario passes", STOPPED_DEFAULT == result && cleanup_counter >= 5);
+
+	SchedDestroy(s);
 }
+
 void TestSchedRunAndStop()
 {
-    sched_t* s = SchedCreate();
-    time_t now = time(NULL);
-    status_t result;
-    reschedule_counter = 3;
+	sched_t* s = SchedCreate();
+	time_t now = time(NULL);
+	status_t result;
+	reschedule_counter = 3;
 
-	printf(TITLE "\nTest: SchedRun() and SchedStop()\n" RESET);
-
-    SchedAddTask(s, now + 1, ActionGetSize, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 2, ActionIsEmpty, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 3, ActionPrintSizeBeforeClear, s, GenericCleanup, NULL, 1);
-    target_uid = SchedAddTask(s, now + 4, ActionRemoveSelf, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 5, ActionCreateScheduler, NULL, GenericCleanup, NULL, 1);
+	TITLE("Test: SchedRun() and SchedStop()");
+	SchedAddTask(s, now + 1, ActionGetSize, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 2, ActionIsEmpty, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 3, ActionPrintSizeBeforeClear, s, GenericCleanup, NULL, 1);
+	target_uid = SchedAddTask(s, now + 4, ActionRemoveSelf, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 5, ActionCreateScheduler, NULL, GenericCleanup, NULL, 1);
 	SchedAddTask(s, now + 6, ActionRescheduleThree, &reschedule_counter, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 10, ActionStopWithoutReschedule, s, GenericCleanup, NULL, 1);
-    
-    result = SchedRun(s);
-    sleep(5);
-    
-    if (STOPPED_BY_USER == result && 0 == reschedule_counter && cleanup_counter >= 5)
-    {
-        printf(GREEN "Test 1 Passed!\n" RESET);
-    }
-    else
-    {
-        printf(RED "Test 1 Failed!\n" RESET);
-        printf("reschedule_counter = %d\n", reschedule_counter);
-		printf("cleanup_counter = %lu\n", cleanup_counter);        
-        printf("result = %d\n", result);
-    }
-    
-    SchedDestroy(s);
+	SchedAddTask(s, now + 10, ActionStopWithoutReschedule, s, GenericCleanup, NULL, 1);
+
+	result = SchedRun(s);
+	sleep(5);
+
+	RUN_TEST("Run-Stop scenario passes", STOPPED_BY_USER == result && cleanup_counter >= 5);
+
+	SchedDestroy(s);
 }
 
 void TestStopWithReschedule()
 {
-    sched_t* s = SchedCreate();
-    time_t now = time(NULL);
-    status_t result;
-	stop_params_t stop_params = {1, NULL};
-	
-	printf(TITLE "\nTest: Stop() with reschedule\n" RESET);
+	sched_t* s = SchedCreate();
+	time_t now = time(NULL);
+	status_t result;
+	stop_params_t stop_params = {0};
+
+	TITLE("Test: Stop() with reschedule");
 
 	stop_params.reschedule_counter = 1;
 	stop_params.s = s;
-	
-    SchedAddTask(s, now + 1, ActionGetSize, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 2, ActionIsEmpty, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 3, ActionStopWithReschedule, &stop_params, GenericCleanup, NULL, 3);
-        
-    SchedAddTask(s, now + 4, ActionGetSize, s, GenericCleanup, NULL, 1);
-    SchedAddTask(s, now + 5, ActionIsEmpty, s, GenericCleanup, NULL, 1);
 
-    result = SchedRun(s);
+	SchedAddTask(s, now + 1, ActionGetSize, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 2, ActionIsEmpty, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 3, ActionStopWithReschedule, &stop_params, GenericCleanup, NULL, 3);
+	SchedAddTask(s, now + 4, ActionGetSize, s, GenericCleanup, NULL, 1);
+	SchedAddTask(s, now + 5, ActionIsEmpty, s, GenericCleanup, NULL, 1);
+
+	result = SchedRun(s);
 	sleep(5);
+	RUN_TEST("Stop with reschedule - 1st run", STOPPED_BY_USER == result);
 
-    if (STOPPED_BY_USER == result)
-    {
-        printf(GREEN "Test 1 Passed!\n" RESET);
-    }
-    else
-    {
-        printf(RED "Test 1 Failed!\n" RESET);
-    }
-    
-    result = SchedRun(s);
-    sleep(5);
-    
-    if (STOPPED_BY_USER == result)
-    {
-        printf(GREEN "Test 2 Passed!\n" RESET);
-    }
-    else
-    {
-        printf(RED "Test 2 Failed!\n" RESET);
-    }
-    
-    SchedDestroy(s);
+	result = SchedRun(s);
+	sleep(5);
+	RUN_TEST("Stop with reschedule - 2nd run", STOPPED_BY_USER == result);
+
+	SchedDestroy(s);
 }
 
 static int ActionGetSize(void* param)
 {
 	sched_t* s = (sched_t*)param;
-	
-	printf("action GetSize: %lu\n", SchedSize(s));
-	
-	return (DONT_RESCHEDULE);
+	printf("Action: GetSize = %lu\n", SchedSize(s));
+	return DONT_RESCHEDULE;
 }
 
 static int ActionIsEmpty(void* param)
 {
 	sched_t* s = (sched_t*)param;
-
-	if (FALSE == SchedIsEmpty(s))
-	{
-		printf("action IsEmpty: FALSE\n");
-	}
-	else
-	{
-		printf("action IsEmpty: TRUE\n");
-	}
-	
-	return (DONT_RESCHEDULE);
+	printf("Action: IsEmpty = %s\n", SchedIsEmpty(s) ? "TRUE" : "FALSE");
+	return DONT_RESCHEDULE;
 }
 
 static int ActionStopWithoutReschedule(void* param)
 {
-	sched_t* s = (sched_t*)param;
-	
-	SchedStop(s);
-	
-	return (DONT_RESCHEDULE);
+	SchedStop((sched_t*)param);
+	return DONT_RESCHEDULE;
 }
 
 static int ActionStopWithReschedule(void* param)
 {
-	stop_params_t* params = (stop_params_t*)param;
-	
-	int counter = params->reschedule_counter;
-	sched_t* s = params->s;
-	
-	SchedStop(s);
-	
-	if (counter > 0)
+	stop_params_t* p = (stop_params_t*)param;
+	SchedStop(p->s);
+
+	if (p->reschedule_counter-- > 0)
 	{
-		params->reschedule_counter--;
-		
-		return (TO_RESCHEDULE);
+		return TO_RESCHEDULE;
 	}
-	
-	return (DONT_RESCHEDULE);
+
+	return DONT_RESCHEDULE;
 }
 
 static int ActionRemoveSelf(void* param)
 {
-    sched_t* s;
-    s = (sched_t*)param;
-    
-    SchedRemoveTask(s, target_uid);
-    
-    return (DONT_RESCHEDULE);
+	SchedRemoveTask((sched_t*)param, target_uid);
+	return DONT_RESCHEDULE;
 }
 
 static int ActionCreateScheduler(void* param)
 {
-    sched_t* new_s;
-    (void)param;
-    new_s = SchedCreate();
-    
-    SchedDestroy(new_s);
-    
-    return (DONT_RESCHEDULE);
+	sched_t* s = SchedCreate();
+	SchedDestroy(s);
+	return DONT_RESCHEDULE;
+	(void)param;
 }
 
 static int ActionRescheduleThree(void* param)
 {
 	int* counter = (int*)param;
-	
-	if (*counter > 0)
+
+	if ((*counter)-- > 0)
 	{
-		(*counter)--;
-		
-		return (TO_RESCHEDULE);
+		return TO_RESCHEDULE;
 	}
-	
-	return (DONT_RESCHEDULE);
+
+	return DONT_RESCHEDULE;
 }
 
 static int ActionPrintSizeBeforeClear(void* param)
 {
-    sched_t* s;
-    s = (sched_t*)param;
-    
-    printf("Before Clear: size = %lu\n", SchedSize(s));
-    
-    return (DONT_RESCHEDULE);
+	printf("Before Clear: size = %lu\n", SchedSize((sched_t*)param));
+	return DONT_RESCHEDULE;
 }
 
 static int ActionClearAllWithPrint(void* param)
 {
-    sched_t* s = (sched_t*)param;
-
-    SchedClear(s);
-    
-    printf("After Clear: size = %lu\n" , SchedSize(s));
-    
-    return (DONT_RESCHEDULE);
+	sched_t* s = (sched_t*)param;
+	SchedClear(s);
+	printf("After Clear: size = %lu\n", SchedSize(s));
+	return DONT_RESCHEDULE;
 }
 
 static void GenericCleanup(void* param)
 {
 	(void)param;
-	
 	cleanup_counter++;
 }
