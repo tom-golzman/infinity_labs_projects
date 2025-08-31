b072fd44cd7d09dd6a1f0e4ad5ff2ecf2f9cdb00
Tom Golzman <169097308+Tom-Golzman@users.noreply.github.com>
2025-08-31 19:52:39 +0300

    update test_utils.h

--- utils/test/test_utils.h
+++ utils/test/test_utils.h
@@ -3,18 +3,35 @@
 
 /************************************colors************************************/
 #define GREEN "\033[1;32m"
-#define RED "\033[1;31m"
+#define RED   "\033[1;31m"
 #define RESET "\033[0m"
 
 /************************************titles************************************/
-#define BOLD "\033[1m"
-#define BOLD_TITLE "\033[1;4m"
-#define TITLE "\033[4m"
+#define BOLD 		"\033[1m"
+#define BOLD_TITLE 	"\033[1;4m"
 
 /************************************status************************************/
 enum { SUCCESS = 0, FAIL = 1 };
 
 /************************************boolean************************************/
 enum { FALSE = 0, TRUE = 1 };
+
+#define RUN_TEST(name, condition)                    	\
+    do {                                             	\
+        if (condition)                               	\
+        {                                            	\
+            printf(GREEN "[PASS]" RESET "%s\n", name); 	\
+        }                                            	\
+        else                                         	\
+        {                                            	\
+            printf(RED "[FAILED]" RESET "%s\n", name); 	\
+        }                                            	\
+    } while (0)
+
+#define TITLE(title)                                 	\
+    do {                                             	\
+        printf("\n");		                            \
+        printf(BOLD_TITLE "%s" RESET "\n", title); 		\
+    } while (0)
 
 #endif /* __TEST_UTILS_H__ */
